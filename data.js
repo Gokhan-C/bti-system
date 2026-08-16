6378cf283f496a41cc47e691cd022f299ea87e5e
Gokhan Cankaya <cankayagok@gmail.com>
2026-08-16 08:02:27 +0300

    chore(site): otomatik güncelleme 2026-08-16

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 window.BTI_DATA = {
- "generated_at": "2026-08-15 09:34",
- "today": "2026-08-15",
- "today_tr": "15 Ağustos 2026",
+ "generated_at": "2026-08-16 08:02",
+ "today": "2026-08-16",
+ "today_tr": "16 Ağustos 2026",
  "latest_date": "2026-08-14",
  "latest_is_today": false,
  "total_decisions": 4256,
